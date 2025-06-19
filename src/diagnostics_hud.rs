d594c26005c25aa049f6c7e839893325768abe32
Emily Strickland <hello@emily.st>
2025-06-18 20:07:27 -0700

    Change HUD refactor for clarity, temporarily

--- src/diagnostics_hud.rs
+++ src/diagnostics_hud.rs
@@ -4,7 +4,6 @@ use bevy::asset::io::AssetSourceId;
 use bevy::asset::AssetPath;
 use bevy::diagnostic::DiagnosticsStore;
 use bevy::diagnostic::FrameTimeDiagnosticsPlugin;
-use bevy::ecs::relationship::RelatedSpawnerCommands;
 use bevy::prelude::*;
 use std::path::Path;
 use std::time::Duration;
@@ -20,28 +19,28 @@ static BOLD_OBLIQUE_OTF_BYTES: &[u8] =
 #[reflect(Resource, Debug)]
 pub struct DiagnosticsHudSettings {
     pub enabled: bool,
-    pub update_interval: Duration,
+    pub refresh_interval: Duration,
 }
 
 impl Default for DiagnosticsHudSettings {
     fn default() -> Self {
         Self {
             enabled: true,
-            update_interval: Duration::from_secs_f64(0.5),
+            refresh_interval: Duration::from_secs_f64(0.5),
         }
     }
 }
 
 #[derive(Resource)]
 pub struct DiagnosticsHudState {
-    pub update_timer: Timer,
+    pub refresh_timer: Timer,
 }
 
 impl Default for DiagnosticsHudState {
     fn default() -> Self {
         Self {
-            update_timer: Timer::new(
-                DiagnosticsHudSettings::default().update_interval,
+            refresh_timer: Timer::new(
+                DiagnosticsHudSettings::default().refresh_interval,
                 TimerMode::Repeating,
             ),
         }
@@ -79,22 +78,14 @@ impl DiagnosticsHudPlugin {
         );
     }
 
-    // fn init_update_timer(
-    //     settings: Res<DiagnosticsHudSettings>,
-    //     mut state: ResMut<DiagnosticsHudState>,
-    // ) {
-    //     state.update_timer = Timer::new(settings.update_interval, TimerMode::Repeating);
-    // }
-
     fn spawn_diagnostics_hud(
         mut commands: Commands,
-        settings: Res<DiagnosticsHudSettings>,
         asset_server: Res<AssetServer>,
+        settings: Res<DiagnosticsHudSettings>,
     ) {
         commands
             .spawn((
                 Name::new("Diagnostics HUD"),
-                DiagnosticsHudUiNode,
                 Node {
                     position_type: PositionType::Absolute,
                     top: Val::Px(5.0),
@@ -109,13 +100,14 @@ impl DiagnosticsHudPlugin {
                     row_gap: Val::Px(1.0),
                     ..default()
                 },
-                BackgroundColor(Color::srgba(0.2, 0.2, 0.2, 0.7)),
+                DiagnosticsHudNode,
                 BorderRadius::all(Val::Px(5.0)),
+                BackgroundColor(Color::srgba(0.2, 0.2, 0.2, 0.7)),
             ))
             .with_children(|commands| {
                 commands
                     .spawn((
-                        DiagnosticsHudGroup,
+                        DiagnosticsHudGroupNode,
                         Node {
                             display: Display::Flex,
                             flex_direction: FlexDirection::Column,
@@ -123,20 +115,37 @@ impl DiagnosticsHudPlugin {
                             ..default()
                         },
                     ))
-                    .with_children(|mut commands| {
-                        hud_row(
-                            &asset_server,
-                            &mut commands,
-                            FpsLabelText,
-                            FpsValueText,
-                            "FPS",
-                            "-",
-                        );
+                    .with_children(|commands| {
+                        commands
+                            .spawn((
+                                DiagnosticsHudRowNode,
+                                Node {
+                                    display: Display::Flex,
+                                    justify_content: JustifyContent::SpaceBetween,
+                                    column_gap: Val::Px(20.0),
+                                    ..default()
+                                },
+                            ))
+                            .with_children(|commands| {
+                                commands.spawn(Node::default()).with_children(|commands| {
+                                    commands.spawn((
+                                        Text::new("FPS"),
+                                        Self::hud_bold_text_font(&asset_server),
+                                    ));
+                                });
+                                commands.spawn(Node::default()).with_children(|commands| {
+                                    commands.spawn((
+                                        FpsValueTextNode,
+                                        Text::new("-"),
+                                        Self::hud_regular_text_font(&asset_server),
+                                    ));
+                                });
+                            });
                     });
 
                 commands
                     .spawn((
-                        DiagnosticsHudGroup,
+                        DiagnosticsHudGroupNode,
                         Node {
                             display: Display::Flex,
                             flex_direction: FlexDirection::Column,
@@ -144,25 +153,66 @@ impl DiagnosticsHudPlugin {
                             ..default()
                         },
                     ))
-                    .with_children(|mut commands| {
-                        hud_row(
-                            &asset_server,
-                            &mut commands,
-                            BarycenterLabelText,
-                            BarycenterValueText,
-                            "Barycenter",
-                            "-",
-                        );
+                    .with_children(|commands| {
+                        commands
+                            .spawn((
+                                DiagnosticsHudRowNode,
+                                Node {
+                                    display: Display::Flex,
+                                    justify_content: JustifyContent::SpaceBetween,
+                                    column_gap: Val::Px(20.0),
+                                    ..default()
+                                },
+                            ))
+                            .with_children(|commands| {
+                                commands.spawn(Node::default()).with_children(|commands| {
+                                    commands.spawn((
+                                        Text::new("Barycenter"),
+                                        Self::hud_bold_text_font(&asset_server),
+                                    ));
+                                });
+                                commands.spawn(Node::default()).with_children(|commands| {
+                                    commands.spawn((
+                                        BarycenterValueTextNode,
+                                        Text::new("-"),
+                                        Self::hud_regular_text_font(&asset_server),
+                                    ));
+                                });
+                            });
                     });
             });
     }
 
+    fn hud_regular_text_font(asset_server: &AssetServer) -> TextFont {
+        let path = Path::new("fonts/BerkeleyMono-Regular");
+        let source = AssetSourceId::from("embedded");
+        let asset_path = AssetPath::from_path(&path).with_source(source);
+
+        TextFont {
+            font: asset_server.load(asset_path),
+            font_size: 12.0,
+            ..default()
+        }
+    }
+
+    fn hud_bold_text_font(asset_server: &AssetServer) -> TextFont {
+        let path = Path::new("fonts/BerkeleyMono-Bold");
+        let source = AssetSourceId::from("embedded");
+        let asset_path = AssetPath::from_path(&path).with_source(source);
+
+        TextFont {
+            font: asset_server.load(asset_path),
+            font_size: 12.0,
+            ..default()
+        }
+    }
+
     fn update_barycenter_value(
         diagnostics: Res<DiagnosticsStore>,
-        mut barycenter_value_text: Single<&mut Text, With<BarycenterValueText>>,
+        mut barycenter_value_text: Single<&mut Text, With<BarycenterValueTextNode>>,
         state: Res<DiagnosticsHudState>,
     ) {
-        if state.update_timer.finished() {
+        if state.refresh_timer.finished() {
             if let (Some(barycenter_x), Some(barycenter_y), Some(barycenter_z)) = (
                 diagnostics.get(&SimulationDiagnosticsPlugin::BARYCENTER_X_PATH),
                 diagnostics.get(&SimulationDiagnosticsPlugin::BARYCENTER_Y_PATH),
@@ -184,10 +234,10 @@ impl DiagnosticsHudPlugin {
 
     fn update_fps_value(
         diagnostics: Res<DiagnosticsStore>,
-        mut fps_hud_value: Single<&mut Text, With<FpsValueText>>,
+        mut fps_hud_value: Single<&mut Text, With<FpsValueTextNode>>,
         state: Res<DiagnosticsHudState>,
     ) {
-        if state.update_timer.finished() {
+        if state.refresh_timer.finished() {
             if let Some(fps) = diagnostics.get(&FrameTimeDiagnosticsPlugin::FPS) {
                 if let Some(fps) = fps.smoothed() {
                     ***fps_hud_value = format!("{fps:.2}");
@@ -196,8 +246,8 @@ impl DiagnosticsHudPlugin {
         }
     }
 
-    fn update_timer_ticks(mut state: ResMut<DiagnosticsHudState>, time: Res<Time>) {
-        state.update_timer.tick(time.delta());
+    fn advance_refresh_timer(mut state: ResMut<DiagnosticsHudState>, time: Res<Time>) {
+        state.refresh_timer.tick(time.delta());
     }
 }
 
@@ -205,6 +255,8 @@ impl Plugin for DiagnosticsHudPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(DiagnosticsHudSettings::default());
         app.insert_resource(DiagnosticsHudState::default());
+        // app.insert_resource(BarycenterNameValuePair::default());
+        // app.insert_resource(FpsNameValuePair::default());
 
         app.add_systems(
             Startup,
@@ -215,136 +267,53 @@ impl Plugin for DiagnosticsHudPlugin {
             (
                 Self::update_fps_value,
                 Self::update_barycenter_value,
-                Self::update_timer_ticks,
+                Self::advance_refresh_timer,
             ),
         );
     }
 }
 
 #[derive(Component, Copy, Clone, Default, PartialEq, Debug)]
-struct DiagnosticsHudUiNode;
+struct DiagnosticsHudNode;
 
 #[derive(Component, Copy, Clone, Default, PartialEq, Debug)]
-struct DiagnosticsHudGroup;
+struct DiagnosticsHudGroupNode;
 
 #[derive(Component, Copy, Clone, Default, PartialEq, Debug)]
-struct DiagnosticsHudRow;
+struct DiagnosticsHudRowNode;
 
 #[derive(Component, Copy, Clone, Default, PartialEq, Debug)]
-struct FpsLabelText;
+struct FpsValueTextNode;
 
 #[derive(Component, Copy, Clone, Default, PartialEq, Debug)]
-struct FpsValueText;
+struct BarycenterValueTextNode;
 
-#[derive(Component, Copy, Clone, Default, PartialEq, Debug)]
-struct BarycenterLabelText;
-
-#[derive(Component, Copy, Clone, Default, PartialEq, Debug)]
-struct BarycenterValueText;
-
-// TODO: continue CommandsExt refactor
-
-// trait CommandsExt {
-//     fn diagnostics_hud_group(&mut self) -> EntityCommands;
-//     fn diagnostics_hud_row(&mut self) -> EntityCommands;
-// }
-
-// impl CommandsExt for RelatedSpawnerCommands<'_, ChildOf> {
-//     fn diagnostics_hud_group(&mut self) -> EntityCommands {
-//         self.spawn((
-//             DiagnosticsHudGroup,
-//             Node {
-//                 display: Display::Flex,
-//                 flex_direction: FlexDirection::Column,
-//                 row_gap: Val::Px(1.0),
-//                 ..default()
-//             },
-//         ))
-//     }
-//
-//     fn diagnostics_hud_row(&mut self) -> EntityCommands {
-//         self.spawn((
-//             DiagnosticsHudRow,
-//             Node {
-//                 display: Display::Flex,
-//                 justify_content: JustifyContent::SpaceBetween,
-//                 column_gap: Val::Px(20.0),
-//                 ..default()
-//             },
-//         ))
-//     }
-// }
-
-fn hud_row<T: Component + Clone, U: Component + Clone>(
-    asset_server: &AssetServer,
-    commands: &mut RelatedSpawnerCommands<ChildOf>,
-    label_marker: T,
-    value_marker: U,
-    label: &str,
-    value: &str,
-) {
-    commands
-        .spawn((
-            DiagnosticsHudRow,
-            Node {
-                display: Display::Flex,
-                justify_content: JustifyContent::SpaceBetween,
-                column_gap: Val::Px(20.0),
-                ..default()
-            },
-        ))
-        .with_children(|mut commands| {
-            hud_label(&asset_server, &mut commands, label_marker, label);
-            hud_value(&asset_server, &mut commands, value_marker, value);
-        });
+#[derive(Resource, Debug)]
+struct FpsNameValuePair {
+    name: String,
+    value: String,
 }
 
-fn hud_label<T: Component + Clone>(
-    asset_server: &AssetServer,
-    commands: &mut RelatedSpawnerCommands<ChildOf>,
-    marker: T,
-    label: &str,
-) {
-    commands.spawn(Node::default()).with_children(|commands| {
-        commands.spawn((marker, Text::new(label), hud_bold_text_font(&asset_server)));
-    });
-}
-
-fn hud_value<T: Component + Clone>(
-    asset_server: &AssetServer,
-    commands: &mut RelatedSpawnerCommands<ChildOf>,
-    marker: T,
-    value: &str,
-) {
-    commands.spawn(Node::default()).with_children(|commands| {
-        commands.spawn((
-            marker,
-            Text::new(value),
-            hud_regular_text_font(&asset_server),
-        ));
-    });
-}
-
-fn hud_regular_text_font(asset_server: &AssetServer) -> TextFont {
-    let path = Path::new("fonts/BerkeleyMono-Regular");
-    let source = AssetSourceId::from("embedded");
-    let asset_path = AssetPath::from_path(&path).with_source(source);
-
-    TextFont {
-        font: asset_server.load(asset_path),
-        font_size: 12.0,
-        ..default()
+impl Default for FpsNameValuePair {
+    fn default() -> Self {
+        Self {
+            name: String::from("FPS"),
+            value: String::from("-"),
+        }
     }
 }
 
-fn hud_bold_text_font(asset_server: &AssetServer) -> TextFont {
-    let path = Path::new("fonts/BerkeleyMono-Bold");
-    let source = AssetSourceId::from("embedded");
-    let asset_path = AssetPath::from_path(&path).with_source(source);
+#[derive(Resource, Debug)]
+struct BarycenterNameValuePair {
+    name: String,
+    value: String,
+}
 
-    TextFont {
-        font: asset_server.load(asset_path),
-        font_size: 12.0,
-        ..default()
+impl Default for BarycenterNameValuePair {
+    fn default() -> Self {
+        Self {
+            name: String::from("Barycenter"),
+            value: String::from("-"),
+        }
     }
 }
