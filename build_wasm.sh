79a4de6a159461baba7a035086770a92de454d88
Emily Strickland <hello@emily.st>
2025-06-21 14:13:14 -0700

    build(wasm): keep original wasm binary with gzipped version

--- build_wasm.sh
+++ build_wasm.sh
@@ -4,8 +4,9 @@ echo "Building for WASM target..."
 
 rustup target add wasm32-unknown-unknown
 
-export RUSTFLAGS='--cfg getrandom_backend="wasm_js"'
-cargo build --profile wasm --target wasm32-unknown-unknown
+RUSTFLAGS='--cfg getrandom_backend="wasm_js"' cargo build \
+  --profile wasm \
+  --target wasm32-unknown-unknown
 
 if ! command -v wasm-bindgen > /dev/null; then
     echo "Installing wasm-bindgen-cli..."
@@ -21,6 +22,6 @@ wasm-bindgen \
     --out-name "many_body_simulation" \
     ./target/wasm32-unknown-unknown/wasm/many_body_simulation.wasm
 
-gzip -f ./out/many_body_simulation_bg.wasm
+gzip --keep --force ./out/many_body_simulation_bg.wasm
 
 cp index.html ./out/
