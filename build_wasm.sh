b02198879adb9e1e1489905f956ea75862e22e75
Emily Strickland <hello@emily.st>
2025-06-21 12:18:14 -0700

    build(wasm): optimize wasm output size and enforce gzip overwrite

--- build_wasm.sh
+++ build_wasm.sh
@@ -21,6 +21,6 @@ wasm-bindgen \
     --out-name "many_body_simulation" \
     ./target/wasm32-unknown-unknown/wasm/many_body_simulation.wasm
 
-gzip ./out/many_body_simulation_bg.wasm
+gzip -f ./out/many_body_simulation_bg.wasm
 
 cp index.html ./out/
