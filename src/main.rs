6cc46c54df9dfdc40cfc4cd4463661896ae1b532
Emily Strickland <hello@emily.st>
2025-06-29 11:59:54 -0700

    feat(core): minimize unneeded I/O and async compute thread tasks
    Add `TaskPoolPlugin` to configure task pools using `available_parallelism`. This ensures the compute threads' minimum matches the system's total available threads, enhancing parallelism handling.

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,15 @@ use crate::plugins::diagnostics_hud::DiagnosticsHudPlugin;
 use crate::plugins::embedded_assets::EmbeddedAssetsPlugin;
 use crate::plugins::simulation::SimulationPlugin;
 use crate::plugins::simulation_diagnostics::SimulationDiagnosticsPlugin;
-use crate::states::{AppState, LoadingState};
+use crate::states::AppState;
+use crate::states::LoadingState;
 use avian3d::prelude::*;
+use bevy::app::TaskPoolThreadAssignmentPolicy;
 use bevy::diagnostic::EntityCountDiagnosticsPlugin;
 use bevy::diagnostic::FrameTimeDiagnosticsPlugin;
 use bevy::diagnostic::SystemInformationDiagnosticsPlugin;
 use bevy::prelude::*;
+use bevy::tasks::available_parallelism;
 use bevy::window::WindowMode;
 use bevy_panorbit_camera::PanOrbitCameraPlugin;
 
@@ -24,16 +27,32 @@ fn main() {
     let mut app = App::new();
 
     app.add_plugins((
-        DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                fullsize_content_view: true,
-                mode: WindowMode::Windowed,
-                title: "Stardrift".to_string(),
-                titlebar_transparent: true,
+        DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    fullsize_content_view: true,
+                    mode: WindowMode::Windowed,
+                    title: "Stardrift".to_string(),
+                    titlebar_transparent: true,
+                    ..default()
+                }),
                 ..default()
+            })
+            .set(TaskPoolPlugin {
+                task_pool_options: TaskPoolOptions {
+                    compute: TaskPoolThreadAssignmentPolicy {
+                        // set the minimum # of compute threads
+                        // to the total number of available threads
+                        min_threads: available_parallelism(),
+                        max_threads: usize::MAX, // unlimited max threads
+                        percent: 1.0,            // this value is irrelevant in this case
+                        on_thread_spawn: None,
+                        on_thread_destroy: None,
+                    },
+                    // keep the defaults for everything else
+                    ..default()
+                },
             }),
-            ..default()
-        }),
         EmbeddedAssetsPlugin,
         DiagnosticsHudPlugin,
         EntityCountDiagnosticsPlugin,
