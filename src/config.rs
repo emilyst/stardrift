28295f1ab5f34fff491d2bdbfbcd48adca525ace
Emily Strickland <hello@emily.st>
2025-07-05 21:43:47 -0700

    refactor(config): update default `gravitational_constant` and `octree_theta` values for WebAssembly

--- src/config.rs
+++ src/config.rs
@@ -46,7 +46,7 @@ impl Default for PhysicsConfig {
     fn default() -> Self {
         if cfg!(target_arch = "wasm32") {
             Self {
-                gravitational_constant: 1e1,
+                gravitational_constant: 1e2,
                 body_count: 100,
                 octree_theta: 1.0,
                 octree_leaf_threshold: 4,
@@ -64,7 +64,7 @@ impl Default for PhysicsConfig {
             Self {
                 gravitational_constant: 1e2,
                 body_count: 1000,
-                octree_theta: 2.0,
+                octree_theta: 1.0,
                 octree_leaf_threshold: 4,
                 body_distribution_sphere_radius_multiplier: 100.0,
                 body_distribution_min_distance: 0.001,
